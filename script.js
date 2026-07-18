36d1e32b3cfcafdfa7f2cfa34f187b134729d011
game2025acc-diali <game2025acc@gmail.com>
2026-07-18 20:27:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ document.getElementById("A").innerHTML = cau.A;
 document.getElementById("B").innerHTML = cau.B;
 document.getElementById("C").innerHTML = cau.C;
 document.getElementById("D").innerHTML = cau.D;
+
+function kiemTra(dapAn) {
+    if (dapAn == cau.dapAn) {
+        document.getElementById("ketQua").innerHTML = "✅ Chính xác!";
+    } else {
+        document.getElementById("ketQua").innerHTML = "❌ Sai rồi!";
+    }
+}
+
+document.getElementById("A").onclick = function() {
+    kiemTra("A");
+};
+
+document.getElementById("B").onclick = function() {
+    kiemTra("B");
+};
+
+document.getElementById("C").onclick = function() {
+    kiemTra("C");
+};
+
+document.getElementById("D").onclick = function() {
+    kiemTra("D");
+};
