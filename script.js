c4232aeb56b8afb28abc24ba0d86be88d412503c
game2025acc-diali <game2025acc@gmail.com>
2026-07-18 20:16:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Game Địa lí 9 đã khởi động");
+var cau = data[0];
+
+document.getElementById("cauHoi").innerHTML = cau.cauHoi;
+
+document.getElementById("A").innerHTML = cau.A;
+document.getElementById("B").innerHTML = cau.B;
+document.getElementById("C").innerHTML = cau.C;
+document.getElementById("D").innerHTML = cau.D;
